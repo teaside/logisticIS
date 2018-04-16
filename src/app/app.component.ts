84a51c72aa214f3af2845fbf07f810e223c0b6ac
DimaRykunov <dima.rikunov@gmail.com>
2018-04-16 15:34:05 +0300

    learn 3 (direcives)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  qwe: any = '';
+  items = [1, 2, 3, 4, 5];
+  headerText = 'Создание своей директивы';
+  current = 1;
+  onClick(number: number) {
+    this.current = number;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { CarsComponent } from './cars/cars.component';
-import { CarComponent } from './car/car.component';
-import { AddCarComponent } from './add-car/add-car.component';
-
+import { BackgroundDirective } from './directives/background.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarsComponent,
-    CarComponent,
-    AddCarComponent
+    BackgroundDirective
   ],
   imports: [
     BrowserModule,
