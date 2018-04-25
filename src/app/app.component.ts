4b06bf5d216fb57660091665090068a19c2518a0
teaside <dima.rykunov@hotmail.com>
2018-04-25 19:57:47 +0300

    http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,17 +12,51 @@ interface Cars {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  colors = ['red', 'blue', 'green', 'yellow', 'pink', 'brown'];
   cars: Cars[] = [];
+  carName = '';
+  appTitle;
 
   constructor(private carsService: CarsService) {}
 
+  ngOnInit() {
+    this.appTitle = this.carsService.getAppTitle();
+  }
+
   loadCars() {
-    this.carsService.
-    getCars()
-    .subscribe((cars: Cars[]) => {
-      this.cars = cars;
+    this.carsService.getCars().subscribe(
+      (cars: Cars[]) => {
+        this.cars = cars;
+      },
+      error => {
+        alert(error);
+      }
+    );
+  }
+
+  addCar() {
+    this.carsService.addCar(this.carName).subscribe((car: Cars) => {
+      this.cars.push(car);
+    });
+    this.carName = '';
+  }
+
+  getRandColor() {
+    const num = Math.round(Math.random() * (this.cars.length - 1));
+    return this.colors[num];
+  }
+
+  setNewColor(car: Cars) {
+    this.carsService.changeColor(car, this.getRandColor()).subscribe(data => {
+      console.log(data);
+    });
+  }
+
+  deleteCar(car: Cars) {
+    this.carsService.deleteCar(car).subscribe(data => {
+      this.cars = this.cars.filter(c => c.id !== car.id);
     });
   }
 }
--- src/app/cars.service.ts
+++ src/app/cars.service.ts
@@ -1,12 +1,52 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, Response, Headers } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class CarsService {
   constructor(private http: Http) {}
 
+  getAppTitle() {
+    return this.http.get('http://localhost:3000/title')
+    .map( (response: Response) => response.json())
+    .map( (data) => data.value);
+  }
   getCars() {
-    return this.http.get('http://localhost:3000/cars')
-    .map((response: Response) => response.json());
+    const headers = new Headers ({
+      'Content-Type': 'application/json; charset=utf8'
+    });
+    return this.http
+    .get('http://localhost:3000/cars', {
+      headers: headers
+    })
+    .map((response: Response) => response.json())
+    .catch( (error: Response) => {
+      return Observable.throw(error);
+    });
+  }
+
+  addCar(carName: string) {
+    const data = {
+      name: carName,
+      color: 'blue'
+    };
+    const headers = new Headers ({
+      'Content-Type': 'application/json; charset=utf8'
+    });
+    return this.http.post('http://localhost:3000/cars', data, {
+      headers
+    })
+    .map((response: Response) => response.json() );
+  }
+
+  changeColor(car: any, color: string) {
+    car.color = color;
+    return this.http.put(`http://localhost:3000/cars/${car.id}`, car)
+    .map( (response: Response) => response.json() );
+  }
+
+  deleteCar(car: any) {
+    return this.http.delete(`http://localhost:3000/cars/${car.id}`)
+    .map( (respsonse: Response) => respsonse.json() );
   }
 }
